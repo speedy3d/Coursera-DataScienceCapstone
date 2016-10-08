abd3cc7735ea813e0d103d6bc6c3f37a35a78ce8
speedy3d <ryan@speedy3d.com>
2016-10-08 08:21:17 -0400

    Changes, additions and updates
    Making progress

--- ReadAndProcess.R
+++ ReadAndProcess.R
@@ -12,43 +12,43 @@ file_blogs = "F:/My Documents/GitHub/Coursera-DataScienceCapstone/SwiftKey_DataF
 file_news = "F:/My Documents/GitHub/Coursera-DataScienceCapstone/SwiftKey_DataFiles/en_US.news.txt"
 file_twitter = "F:/My Documents/GitHub/Coursera-DataScienceCapstone/SwiftKey_DataFiles/en_US.twitter.txt"
 
-#Create clean corpus
-  #Use small subset of all three datasets to stay under the appropriate filesize for shiny, this also greatly speeds up processing, will use 10% of each file
-  sample_blogs <- readLines(file_blogs, (lines_blogs*0.10), encoding='UTF-8', warn=FALSE, skipNul=TRUE)
-  sample_news <- readLines(file_news, (lines_news*0.10), encoding='UTF-8', warn=FALSE, skipNul=TRUE)
-  sample_twitter <- readLines(file_twitter, (lines_twitter*0.10), encoding='UTF-8', warn=FALSE, skipNul=TRUE)
+#Create the clean corpus
+#Use small subset of all three datasets to stay under the appropriate filesize for shiny, this also greatly speeds up processing, will use 10% of each file
+sample_blogs <- readLines(file_blogs, (lines_blogs*0.10), encoding='UTF-8', warn=FALSE, skipNul=TRUE)
+sample_news <- readLines(file_news, (lines_news*0.10), encoding='UTF-8', warn=FALSE, skipNul=TRUE)
+sample_twitter <- readLines(file_twitter, (lines_twitter*0.10), encoding='UTF-8', warn=FALSE, skipNul=TRUE)
+
+#Merge the three data sets
+dataset_sample <- c(sample_blogs, sample_news, sample_twitter)
+
+#Create a clean corpus sample of all three files
+corpus_vector <- VectorSource(dataset_sample)
+clean_corpus <- Corpus(corpus_vector)
+
+#Make sure encoding is correct and clean some issues related to emojis in the dataset
+clean_corpus <- tm_map(clean_corpus, function(x) iconv(x, to='UTF-8', sub='byte'))
+
+#Clean punctuation marks
+clean_corpus <- tm_map(clean_corpus, removePunctuation)
+
+#Remove numbers
+clean_corpus <- tm_map(clean_corpus, removeNumbers)
+
+#Remove extra whitespace
+clean_corpus <- tm_map (clean_corpus, stripWhitespace)
+
+#Remove english stopwords, this is probably not needed but I will leave this code here
+#clean_corpus <- tm_map(clean_corpus, removeWords, stopwords("english")) 
+
+#Convert all letters to lowercase
+clean_corpus <- tm_map(clean_corpus, tolower)
+
+#Convert to a plaintext document, this should not be needed
+#clean_corpus <- tm_map(clean_corpus, PlainTextDocument)
+
   
-  #Merge the three data sets
-  dataset_sample <- c(sample_blogs, sample_news, sample_twitter)
-  
-  #Create a clean corpus sample of all three files
-  corpus_vector <- VectorSource(dataset_sample)
-  clean_corpus <- Corpus(corpus_vector)
-  
-  #Make sure encoding is correct and clean some issues related to emojis in the dataset
-  clean_corpus <- tm_map(clean_corpus, function(x) iconv(x, to='UTF-8', sub='byte'))
-  
-  #Clean punctuation marks
-  clean_corpus <- tm_map(clean_corpus, removePunctuation)
-  
-  #Remove numbers
-  clean_corpus <- tm_map(clean_corpus, removeNumbers)
-  
-  #Remove extra whitespace
-  clean_corpus <- tm_map (clean_corpus, stripWhitespace)
-  
-  #Remove english stopwords, this is probably not needed but I will leave this code here
-  #clean_corpus <- tm_map(clean_corpus, removeWords, stopwords("english")) 
-  
-  #Convert all letters to lowercase
-  clean_corpus <- tm_map(clean_corpus, tolower)
-  
-  #Convert to a plaintext document, this should not be needed
-  #clean_corpus <- tm_map(clean_corpus, PlainTextDocument)
-  
-#Create term document matrix
-  #Bigrams
-  tdm_bigram <- TermDocumentMatrix(clean_corpus, control = list(tokenize = BigramTokenizer)) 
+#Create term document matrix for bigrams, trigrams, and quadgrams
+tdm_bigram <- TermDocumentMatrix(clean_corpus, control = list(tokenize = BigramTokenizer)) 
   
   
 #Create SQL database
